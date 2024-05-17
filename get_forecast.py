9b49de8a6ecc7fbe946a6acf4daae8035d5ad87e
Bean Beret <beanberet@protonmail.com>
2024-05-17 11:52:16 +0200

    move Kp threshold to top of script

--- get_forecast.py
+++ get_forecast.py
@@ -1,6 +1,9 @@
 from sendgrid_mail import sendgrid_send
 from noaa_parser import get_data
 
+# Kp value threshold from when to send an email
+KP_THRESHOLD = 5
+
 # specify email here
 recipient_emails = [
     'THE EMAIL OF THE PERSON WHO WILL GET THE EMAIL GOES HERE',
@@ -19,10 +22,9 @@ email_endpoints = [
 df = get_data()
 # find the max Kp value over the next 3 days:
 max_value = df['value'].max()
-print(f"max_value={max_value}")
 
 # If the forecast Kp index will be equal to or above 5 at any point in the next 3 days, send an email alert using the "SendGrid" service:
-if max_value >= 5:
+if max_value >= KP_THRESHOLD:
     email_content = ("\nAt " + str(df.iloc[0]['time']) + ' on ' + str(df.iloc[0]['variable']) +
                      ' the Kp index is forecast to be ' + str(df.iloc[0]['value']) + '!' +
                      '\n\n\nFor more details, visit:\n\n' + 'https://services.swpc.noaa.gov/text/3-day-forecast.txt')
